0f236c0847c1e36ef7d995b6a7bc6c5427aeec42
Simon Anliker <anliksim@students.zhaw.ch>
2017-09-13 23:27:42 +0200

    s01 a2 count chars and words

---
+++ s01-count/tests/tests.c
@@ -0,0 +1,148 @@
+/* ----------------------------------------------------------------------------
+ * --  _____       ______  _____                                              -
+ * -- |_   _|     |  ____|/ ____|                                             -
+ * --   | |  _ __ | |__  | (___    Institute of Embedded Systems              -
+ * --   | | | '_ \|  __|  \___ \   Zuercher Hochschule Winterthur             -
+ * --  _| |_| | | | |____ ____) |  (University of Applied Sciences)           -
+ * -- |_____|_| |_|______|_____/   8401 Winterthur, Switzerland               -
+ * ----------------------------------------------------------------------------
+ */
+/**
+ * @file
+ * @brief Test suite for the given package.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "CUnit/Basic.h"
+#include "test_utils.h"
+
+#ifndef TARGET // must be given by the make file --> see test target
+#error missing TARGET define
+#endif
+
+/// @brief The name of the STDOUT text file.
+#define OUTFILE "stdout.txt"
+/// @brief The name of the STDERR text file.
+#define ERRFILE "stderr.txt"
+
+// setup & cleanup
+static int setup(void)
+{
+	remove_file_if_exists(OUTFILE);
+	remove_file_if_exists(ERRFILE);
+	return 0; // success
+}
+
+static int teardown(void)
+{
+	// Do nothing.
+	// Especially: do not remove result files - they are removed in int setup(void) *before* running a test.
+	return 0; // success
+}
+
+// tests
+static void test_space_input(void)
+{
+	const char *out_txt[] = {
+		"User input:  \n",
+		"Char count: 1\n",
+		"Word count: 0\n"
+	};
+	const char *err_txt[] = {};
+
+	int exit_code = system("echo ' ' | " 
+		XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	
+	CU_ASSERT_EQUAL(exit_code, 0);
+
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+}
+
+static void test_no_input(void)
+{
+	const char *out_txt[] = {
+		"User input: \n",
+		"Char count: 0\n",
+		"Word count: 0\n"
+	};
+	const char *err_txt[] = {};
+
+	int exit_code = system("echo | " 
+		XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	
+	CU_ASSERT_EQUAL(exit_code, 0);
+
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+}
+
+static void test_words_with_space(void)
+{
+	const char *out_txt[] = {
+		"User input: test this\n",
+		"Char count: 9\n",
+		"Word count: 2\n"
+	};
+	const char *err_txt[] = {};
+
+	int exit_code = system("echo 'test this' | " 
+		XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	
+	CU_ASSERT_EQUAL(exit_code, 0);
+
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+}
+
+static void test_words_with_tab(void)
+{
+	const char *out_txt[] = {
+		"User input: test	this\n",
+		"Char count: 9\n",
+		"Word count: 2\n"
+	};
+	const char *err_txt[] = {};
+
+	int exit_code = system("echo 'test	this' | " 
+		XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	
+	CU_ASSERT_EQUAL(exit_code, 0);
+
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+}
+
+static void test_words_with_space_at_end(void)
+{
+	const char *out_txt[] = {
+		"User input: one two	three \n",
+		"Char count: 14\n",
+		"Word count: 3\n"
+	};
+	const char *err_txt[] = {};
+
+	int exit_code = system("echo 'one two	three ' | " 
+		XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	
+	CU_ASSERT_EQUAL(exit_code, 0);
+
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+}
+
+/*
+ * @brief Registers and runs the tests.
+ */
+int main(void)
+{
+	// setup, run, teardown
+	TestMainBasic("Char and word count",
+		setup, teardown,
+		test_space_input,
+		test_no_input,
+		test_words_with_space,
+		test_words_with_tab,
+		test_words_with_space_at_end
+	);
+}
