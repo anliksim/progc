6ee4e89eedd90c3a556e4e5027c5c74961c55ccd
Simon Anliker <anliksim@students.zhaw.ch>
2017-09-24 17:03:08 +0200

    Add code comments

--- s01-count/src/main.c
+++ s01-count/src/main.c
@@ -13,6 +13,14 @@ int isDelimiter(int input) {
     return isTab(input) || isSpace(input);
 }
 
+/**
+ * Reads a line and counts characters and words.
+ *
+ * If an input contains multiple delimiters between words or
+ * additional delimiters before and after, the count is still correct.
+ *
+ * @return exit code
+ */
 int main() {
 
     int charCount = 0;
@@ -22,6 +30,10 @@ int main() {
     int in;
     while ((in = getchar()) != '\n') {
 
+        /*
+         * Exit with failure on EOF as we do not expect it
+         * and it is an indicator for errors
+         */
         if (in == EOF) {
             return EXIT_FAILURE;
         }
@@ -29,14 +41,13 @@ int main() {
         ++charCount;
 
         /*
-         * Use a swap variable for fault tolerance.
-         * If an input contains multiple delimiters between words or
-         * additional delimiters before and after, the count is still correct.
+         * Count as word if a char is coming after a delimiter
+         * or if it is the first char in line
          */
-        if (!isDelimiter(in) & swap) {
+        if (~isDelimiter(in) & swap) {
             ++wordCount;
             swap = swap ^ 1;
-        } else if (isDelimiter(in) & !swap) {
+        } else if (isDelimiter(in) & ~swap) {
             swap = swap ^ 1;
         }
     }
