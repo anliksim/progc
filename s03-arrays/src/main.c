f9e001403328462f05191b0265a20da5fdf1db84
Simon Anliker <anliksim@students.zhaw.ch>
2017-10-01 22:47:40 +0200

    s03 Mark stats

--- s03-arrays/src/main.c
+++ s03-arrays/src/main.c
@@ -2,7 +2,37 @@
 #include <stdlib.h>
 #include "marks.h"
 
+typedef struct stats {
+    double best;
+    double worst;
+    double average;
+    int avg_count;
+} Stats;
 
+Stats mark_stats = {0};
+
+void updateBest(double mark) {
+    if (mark > mark_stats.best) {
+        mark_stats.best = mark;
+    }
+}
+
+void updateWorst(double mark) {
+    double worst = mark_stats.worst;
+    if (worst == 0 || mark < worst) {
+        mark_stats.worst = mark;
+    }
+}
+
+void updateAverage(double mark) {
+    mark_stats.average = (mark_stats.average * mark_stats.avg_count + mark) / ++mark_stats.avg_count;
+}
+
+void updateStats(double mark) {
+    updateBest(mark);
+    updateWorst(mark);
+    updateAverage(mark);
+}
 
 
 /**
@@ -11,9 +41,10 @@
  */
 int main() {
 
-
+    int gtfour = 0;
     int input[100];
     int i = -1;
+    int max = 0;
 
     do {
         ++i;
@@ -24,14 +55,36 @@ int main() {
     } while (input[i] != -1 && i < 100);
 
 
-    (void) printf("Max:\n");
-    int res = scanf("%d", &input[i]);
-    if (res < 0) {
-        return EXIT_FAILURE;
-    }
+    int print = 1;
+    while(print) {
 
-    for (int j = 0; j <= i; ++j) {
-        (void) printf("%d\n", input[j]);
-    }
+        (void) printf("Max:");
+        int res = scanf("%d", &max);
+        if (res < 0) {
+            return EXIT_FAILURE;
+        }
+
+        for (int j = 0; j < i; ++j) {
+            updateStats(calc_mark(input[j], max));
+            (void) printf("%d\n", input[j]);
+        }
+
+        (void) printf("Statistics (%d students, %d points needed for mark 6)\n", i, input[0]);
+
+        for (int k = 1; k <= 6; ++k) {
+            (void) printf("Mark %d: x\n", k);
+        }
+
+        (void) printf("Best mark: %1.0lf\n", mark_stats.best);
+        (void) printf("Worst mark: %1.0lf\n", mark_stats.worst);
+        (void) printf("Average mark: %1.0lf\n", mark_stats.average);
+        (void) printf("Mark >= 4: %d students (80.00)\n", gtfour);
+
+        res = scanf("%d", &print);
+        if (res < 0) {
+            return EXIT_FAILURE;
+        }
+    };
+
     return EXIT_SUCCESS;
 }
